7809e5a2a08d3f04e628ae0ff5d679fc243bcffb
Can Yolal <canyolal16@gmail.com>
2022-10-02 21:29:05 +0300

    Custom logs supports HTTP error by defining new http.Server struct.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -49,11 +49,22 @@ func main() {
 	mux.HandleFunc("snippet/view", snippetView)
 	mux.HandleFunc("snippet/create", snippetCreate)
 
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set
+	// the ErrorLog field so that the server now uses the custom errorLog logger in
+	// the event of any problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// The value returned from the flag.String() function is a pointer to the flag
 	// value, not the value itself. So we need to dereference the pointer (i.e.
 	// prefix it with the * symbol) before using it. Note that we're using the
 	// log.Printf() function to interpolate the address with the log message.
 	infoLog.Printf("Server is running on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	// Call the ListenAndServe() method on our new http.Server struct.
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
