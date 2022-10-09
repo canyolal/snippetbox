fae3d1a11486763b34c458eafd6b3246221afb6a
Can Yolal <canyolal16@gmail.com>
2022-10-09 15:47:15 +0300

    snippetCreateForm uses internal/validator now

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/canyolal/snippetbox/internal/models"
+	"github.com/canyolal/snippetbox/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -18,10 +17,10 @@ import (
 // must be exported in order to be read by the html/template package when
 // rendering the template.
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 // Change the signature of the home handler so it is defined as a method against
@@ -97,33 +96,27 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	// Create an instance of snippetCreateForm struct containing the values
 	// from the form and an empty map for any validation errors.
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	// Update the validation checks so that they operate on snippetCreateForm instance.
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank."
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters."
-	}
+	// Because the Validator type is embedded by the snippetCreateForm struct,
+	// we can call CheckField() directly on it to execute our validation checks.
+	// CheckField() will add the provided key and error message to the
+	// FieldErrors map if the check does not evaluate to true. For example, in
+	// the first line here we "check that the form.Title field is not blank". In
+	// the second, we "check that the form.Title field has a maximum character
+	// length of 100" and so on.
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank.")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters.")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank.")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1,7, or 365.")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank."
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1,7, or 365."
-		fmt.Fprint(w, form.FieldErrors)
-	}
-
-	// If there are any validation errors re-display the create.tmpl template,
-	// passing in the snippetCreateForm instance as dynamic data in the Form
-	// field. Note that we use the HTTP status code 422 Unprocessable Entity
-	// when sending the response to indicate that there was a validation error.
-	if len(form.FieldErrors) > 0 {
+	// Use the Valid() method to see if any of the checks failed. If they did,
+	// then re-render the template passing in the form in the same way as
+	// before.
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.html", data)
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -37,17 +37,17 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 }
 
 // NotBlank() returns true if a value is not an empty string.
-func (v *Validator) NotBlank(value string) bool {
+func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
 // MaxChars() returns true if a value contains no more than n chararacters.
-func (v *Validator) MaxChars(value string, n int) bool {
+func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
 // PermittedInt() returns true if a value is in a list of permitted integers.
-func (v *Validator) PermittedInt(value int, permittedValues ...int) bool {
+func PermittedInt(value int, permittedValues ...int) bool {
 	for i := range permittedValues {
 		if permittedValues[i] == value {
 			return true
