465ba38429b6709f200255944df91661cbdc3ee1
Can Yolal <canyolal16@gmail.com>
2022-10-11 15:57:29 +0300

    Only elliptic curves with assembly implementations are configured in TLSConfig

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -102,15 +103,23 @@ func main() {
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
 	}
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	// want the server to use. In this case the only thing that we're changing
+	// is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
 
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
 	// the event of any problems.
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(), //Call the app.routes() to get our servemux routes.
+		Addr:      *addr,
+		ErrorLog:  errorLog,
+		Handler:   app.routes(), //Call the app.routes() to get our servemux routes.
+		TLSConfig: tlsConfig,
 	}
 
 	// The value returned from the flag.String() function is a pointer to the flag
